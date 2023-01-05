27b1f6d0e49b1724ce7c8516afc307945df53ee5
rahulsahay19 <rahulsahay19@hotmail.com>
2023-01-06 01:24:56 +0530

    helm chart changes

---
+++ Deployments/helm/deploy-all.ps1
@@ -0,0 +1,51 @@
+Param(
+    [parameter(Mandatory=$false)][string]$appName="eshop",
+    [parameter(Mandatory=$false)][bool]$clean=$true
+    )
+
+function Install-Chart  {
+    Param([string]$chart, [string]$initialOptions)
+    $options=$initialOptions
+
+    $command = "helm install $appName-$chart $chart $options"
+    Write-Host "Helm Command: $command" -ForegroundColor Gray
+    Invoke-Expression $command
+}
+
+
+Write-Host "Begin installation using Helm" -ForegroundColor Green
+
+if ($clean) {    
+    $listOfReleases=$(helm ls --filter $appName -q)    
+    if ([string]::IsNullOrEmpty($listOfReleases)) {
+        Write-Host "No previous releases found!" -ForegroundColor Green
+	}else{
+        Write-Host "Previous releases found" -ForegroundColor Green
+        Write-Host "Cleaning previous helm releases..." -ForegroundColor Green
+        helm uninstall $listOfReleases
+
+        Write-Host "Previous releases deleted" -ForegroundColor Green
+	}        
+}
+
+$infras = ("basketdb", "catalogdb", "discountdb", "elasticsearch", "kibana", "orderdb", "rabbitmq")
+$apis = ("basket","catalog", "ordering", "discount-grpc")
+# $gateways = ("ocelotapigw", "shoppingaggregator")
+
+foreach ($infra in $infras) {
+    Write-Host "Installing infrastructure: $infra" -ForegroundColor Green
+    Write-Host "$infra"
+    Install-Chart $infra
+}
+
+foreach ($api in $apis) {
+    Write-Host "Installing: $api" -ForegroundColor Green
+    Install-Chart $api
+}
+
+# foreach ($chart in $gateways) {
+#     Write-Host "Installing Api Gateway Chart: $chart" -ForegroundColor Green
+#     Install-Chart $chart
+# }
+
+Write-Host "helm charts installed." -ForegroundColor Green
