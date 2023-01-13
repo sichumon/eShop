c569cb2e4ac9e60374c06a6f87fe3a9daeeb8be0
rahulsahay19 <rahulsahay19@hotmail.com>
2023-01-13 12:19:09 +0530

    Added Error Interceptor

--- Client/src/app/core/core.module.ts
+++ Client/src/app/core/core.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
+import { ServerErrorComponent } from './server-error/server-error.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 
 @NgModule({
   declarations: [
-    NavBarComponent
+    NavBarComponent,
+    ServerErrorComponent,
+    NotFoundComponent
   ],
   imports: [
     CommonModule,
